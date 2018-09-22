01d70ae54ad404293b8157651c7f12272fec3d42
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-09-21 21:00:52 -0700

    css stuff

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <header>
       <img class="banner" src="https://pre00.deviantart.net/856e/th/pre/f/2012/263/3/e/3ececa154d5e33543bac7947aba5dad6-d5fc6os.jpg" alt="Handshake with the Devil Banner">
-      <h1>Shady Ventures</h1>
+      <h1>Hazy Ventures</h1>
     </header>
     <button @click="handleSubmit">Generate Your Shady Busniess Idea</button>
     <h2 v-if="pitch">Your website is: {{ pitch.url }}</h2>
